2871f9959997a84e7918ecc22a5c103444927914
JoshuaHolloway <jhollow6@asu.edu>
2021-05-10 14:27:54 -0500

    complete scss compilation setup

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ const sass = require('gulp-sass');
 const autoprefixer = require("gulp-autoprefixer");
 const replace = require("gulp-replace");
 const rename = require("gulp-rename");
+// const run = require('gulp-run-command').default;
+// import run from 'gulp-run-command';
 
 // ==============================================
 
 gulp.task('sass', function() {
-  return gulp.src('./styles/custom-styles.scss')                         // -source
+  return gulp.src('./src/scss/main.scss')                         // -source
     // .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError)) 
     .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))  // -compile with sasss
     .pipe(autoprefixer())                                                // -add vendoer prefixes
@@ -18,22 +20,26 @@ gulp.task('sass', function() {
     .pipe(replace('/*', ""))
     .pipe(replace('*/', ""))
     // .pipe(cleanCSS({compatibility: 'ie11'}))                          // -minifiy
-    .pipe(rename("custom-styles.css.liquid"))                                   // -rename our output to a .liquid file
+    .pipe(rename("main.css.liquid"))                                   // -rename our output to a .liquid file
     .pipe(gulp.dest('./assets'));                                        // -destination
 });
 
 // ==============================================
 
 gulp.task('watch', function() {
-
-  gulp.watch('./styles/**/*.scss', gulp.series('sass'));
-  
   // Upload all files to make sure synced (one way at least) with store
   themeKit.command('deploy', {
     allowLive: true,
     env: 'development' // env corresponds to environment from config.yml
   });
+  
+  // compile js modules
+  // gulp.series(run('webpack --watch'));
 
+  // compile scss ( -> css.liquid )
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+  
+  // Watch for file changes to upload to store
   themeKit.command('watch', {
     allowLive: true,
     env: 'development' // env corresponds to environment from config.yml
@@ -41,4 +47,4 @@ gulp.task('watch', function() {
 
 });
 
-// https://adambohannon.me/shopify-development-using-liquid-syntax-in-sass-and-compiling-with-gulp
+// Adapted from: https://adambohannon.me/shopify-development-using-liquid-syntax-in-sass-and-compiling-with-gulp
