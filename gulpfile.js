0c366bfb1a5f6aa4555a541428a410ba89bf3f59
JoshuaHolloway <jhollow6@asu.edu>
2021-05-10 12:47:37 -0500

    before gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const themeKit = require('@shopify/themekit');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
 // ==============================================
 
 gulp.task('sass', function() {
   return gulp.src('styles/custom-styles.scss') // source
     .pipe(sass())                              // compile with sasss
+    .pipe(cleanCSS({compatibility: 'ie11'}))
     .pipe(gulp.dest('assets'));                // destination
 });
 
